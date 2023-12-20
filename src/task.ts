25e7fa14cb29c799bff50b72933af5bec40771d4
asheque33 <ashequrrahman3333@gmail.com>
2023-12-20 23:57:37 +0600

    project

--- src/task.ts
+++ src/task.ts
@@ -1,5 +1,5 @@
 {
-  // problem-01
+  //
   type Something = string | number;
   const named = (amazing: Something) => {
     if (typeof amazing === "string") {
@@ -13,13 +13,13 @@
 
   interface Person {
     address?: {
-      city: string;
-      street: string;
+      city?: string;
+      street?: string;
     };
     phone?: string;
   }
 
-  const person: Person = {
+  const Rifat: Person = {
     address: {
       city: "Chandpur",
       street: "Zoor Pukur Paar",
@@ -27,11 +27,53 @@
     phone: "01000669542",
   };
 
-  function getAddressCity(obj: Person) {
-    const requiredCity = person?.address?.city;
+  function getAddressCity(person: Person): string | undefined {
+    return person.address?.city;
   }
-  // getAddressCity();
-  //   console.log(person?.address?.city);
+  getAddressCity(Rifat);
+  // console.log(getAddressCity(Rifat));
+
+  const mixedData: (number | string)[] = [
+    "seeds",
+    84,
+    "nuts",
+    23,
+    "salads",
+    67,
+  ];
+
+  function sumNums(mixedData: (number | string)[]): number {
+    let total = 0;
+    mixedData.forEach((data) => {
+      if (typeof data === "number") {
+        total = total + data;
+      }
+    });
+    return total;
+  }
+  sumNums(mixedData);
+  // console.log(sumNums(mixedData));
+
+  interface Car {
+    make: string;
+    model: string;
+    year: number;
+  }
+  interface Driver {
+    name: string;
+    licenseNumber: string;
+  }
+  const carInfo: Car = { make: "Toyota", model: "Murry", year: 2021 };
+  const driverInfo: Driver = { name: "Rifat ", licenseNumber: "123rft" };
+
+  function combineBothCarDriver(
+    car: Car,
+    driver: Driver
+  ): { car: Car; driver: Driver } {
+    return { car, driver };
+  }
+  combineBothCarDriver(carInfo, driverInfo);
+  // console.log(combineBothCarDriver(carInfo, driverInfo));
 
   //
 }
